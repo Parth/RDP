2fbed87c77bfa486ee0041d9296ac305953c06a4
Frederick Lau <fredericklau@nbp-114-244.nbp.ruw.rutgers.edu>
2017-10-25 15:28:26 -0400

    added guide

--- sock352.py
+++ sock352.py
@@ -1,4 +1,3 @@
-
 import binascii
 import socket as syssock
 import struct
@@ -20,12 +19,22 @@ class socket:
         return 
 
     def connect(self,address):  # fill in your code here 
+    #bind
+    #Create SYN Header
+    # start timer
+    #recv SYN ACK B
+    #send ACK C
+    #if there is error send header again
         return 
     
     def listen(self,backlog):
+    pass
         return
 
     def accept(self):
+    #recv Syn A
+    #send SYN ACK B
+    # ACK C
         (clientsocket, address) = (1,1)  # change this to your code 
         return (clientsocket,address)
     
@@ -33,14 +42,16 @@ class socket:
         return 
 
     def send(self,buffer):
+    #create header
+    #send data
+    #start timer
+    #if timeout, send same packet again
+    #
         bytessent = 0     # fill in your code here 
         return bytesent 
 
     def recv(self,nbytes):
+    #recv packet
+    #send right ACK
         bytesreceived = 0     # fill in your code here
         return bytesreceived 
-
-
-    
-
-
